7fe0c9dc758b6c8ba371791498e7f86428fd2fca
manukris <manukrishnatesro@gmail.com>
2019-02-05 14:33:08 +0530

    file hash folder recursion

--- Filehash.py
+++ Filehash.py
@@ -6,34 +6,41 @@ import os
 
 
 
+def checkfolder(folder):
 
-def getReadableHash(filename):
-    with open(filename, "rb") as f:
-        bytes = f.read()  # read entire file as bytes
-        readable_hash = hashlib.md5(bytes).hexdigest()
-        return readable_hash
+    def getReadableHash(filename):
+        with open(filename, "rb") as f:
+            bytes = f.read()  # read entire file as bytes
+            readable_hash = hashlib.md5(bytes).hexdigest()
+            return readable_hash
 
-def getSha256Hash(fileString):
-    readablehash = hashlib.sha3_256(fileString).hexdigest()
-    return readablehash
-
-
-folder = "testdir"
-
-with os.scandir(folder) as dir_entries:
-    for entry in dir_entries:
-        info = entry.stat()
-        name = entry.name
-        filename = folder +"/"+ name
-        hash = getReadableHash(filename)
-        print(hash)
-        print(info)
-        fileString = hash + str(info.st_size) + str(info.st_mtime) + filename
-        print(fileString)
-        fileString = fileString.encode('utf-8')
-        print(fileString)
-        finalHash = getSha256Hash(fileString)
-        print(finalHash)
+    def getSha256Hash(fileString):
+        readablehash = hashlib.sha3_256(fileString).hexdigest()
+        return readablehash
 
 
+    with os.scandir(folder) as dir_entries:
+        for entry in dir_entries:
+            info = entry.stat()
+            name = entry.name
+            filename = folder +"/"+ name
+            if(os.path.isdir(filename)):
+                print("folder")
+                if os.listdir(filename):
+                    checkfolder(filename)
+                else:
+                    print("empty")
+            else:
+                print("file")
+                hash = getReadableHash(filename)
+                print(hash)
+                print(info)
+                fileString = hash + str(info.st_size) + str(info.st_mtime) + filename
+                print(fileString)
+                fileString = fileString.encode('utf-8')
+                print(fileString)
+                finalHash = getSha256Hash(fileString)
+                print(finalHash)
+
+checkfolder('testdir')
 
