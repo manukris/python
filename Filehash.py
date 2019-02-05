2a9e92a9a11bb01b94677ca58e11aa43a9e60254
manukris <manukrishnatesro@gmail.com>
2019-02-05 14:16:05 +0530

    file hash updated

--- Filehash.py
+++ Filehash.py
@@ -5,18 +5,35 @@ import hashlib
 import os
 
 
-f = open("text.txt","wb")
 
-with open("text.txt","rb") as f:
-    bytes = f.read() # read entire file as bytes
-    readable_hash = hashlib.md5(bytes).hexdigest()
-    print(readable_hash)
 
-from pathlib import Path
+def getReadableHash(filename):
+    with open(filename, "rb") as f:
+        bytes = f.read()  # read entire file as bytes
+        readable_hash = hashlib.md5(bytes).hexdigest()
+        return readable_hash
 
-for path in Path('.').iterdir():
-    info = path.stat()
-    print(info)
+def getSha256Hash(fileString):
+    readablehash = hashlib.sha3_256(fileString).hexdigest()
+    return readablehash
+
+
+folder = "testdir"
+
+with os.scandir(folder) as dir_entries:
+    for entry in dir_entries:
+        info = entry.stat()
+        name = entry.name
+        filename = folder +"/"+ name
+        hash = getReadableHash(filename)
+        print(hash)
+        print(info)
+        fileString = hash + str(info.st_size) + str(info.st_mtime) + filename
+        print(fileString)
+        fileString = fileString.encode('utf-8')
+        print(fileString)
+        finalHash = getSha256Hash(fileString)
+        print(finalHash)
 
 
 
