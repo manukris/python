5d47a7a85d09298b10ecbf63b0d83cc70b79a5ad
manukris <manukrishnatesro@gmail.com>
2019-02-05 15:19:20 +0530

    file hash folder recursion sqlite addition

--- Filehash.py
+++ Filehash.py
@@ -3,8 +3,7 @@ import hashlib
 # Python program to find SHA256 hexadecimal hash string of a file
 import hashlib
 import os
-
-
+import sqlite3
 
 def checkfolder(folder):
 
@@ -42,5 +41,23 @@ def checkfolder(folder):
                 finalHash = getSha256Hash(fileString)
                 print(finalHash)
 
-checkfolder('testdir')
+#checkfolder('testdir')
 
+def sqliteops():
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+
+    # Create table
+    c.execute('''CREATE TABLE stocks
+                                 (date text, trans text, symbol text, qty real, price real)''')
+
+    # Insert a row of data
+    c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+    # Save (commit) the changes
+    conn.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
+sqliteops()
