c7ef0fd8ed3dc771e287b4ae32211fd4d94c5b11
manukris <manukrishnatesro@gmail.com>
2019-02-12 12:35:03 +0530

    file hash oops

--- Filehash.py
+++ Filehash.py
@@ -5,50 +5,46 @@ import hashlib
 import os
 import sqlops
 
-def checkfolder(folder):
+class FileHash:
 
-    def getReadableHash(filename):
+    def getReadableHash(self,filename):
         with open(filename, "rb") as f:
             bytes = f.read()  # read entire file as bytes
             readable_hash = hashlib.md5(bytes).hexdigest()
             return readable_hash
 
-    def getSha256Hash(fileString):
+    def getSha256Hash(self,fileString):
         readablehash = hashlib.sha3_256(fileString).hexdigest()
         return readablehash
 
-
-    with os.scandir(folder) as dir_entries:
-        for entry in dir_entries:
-            info = entry.stat()
-            name = entry.name
-            filename = folder +"/"+ name
-            if(os.path.isdir(filename)):
-                print("folder")
-                if os.listdir(filename):
-                    checkfolder(filename)
+    def checkfolder(self,folder):
+        with os.scandir(folder) as dir_entries:
+            for entry in dir_entries:
+                info = entry.stat()
+                name = entry.name
+                filename = folder +"/"+ name
+                if(os.path.isdir(filename)):
+                    print("folder")
+                    if os.listdir(filename):
+                        self.checkfolder(filename)
+                    else:
+                        print("empty")
                 else:
-                    print("empty")
-            else:
-                print("file")
-                hash = getReadableHash(filename)
-                print(hash)
-                print(info)
-                fileString = hash + str(info.st_size) + str(info.st_mtime) + filename
-                print(fileString)
-                fileString = fileString.encode('utf-8')
-                print(fileString)
-                finalHash = getSha256Hash(fileString)
-                print(finalHash)
-                sqlops.sqlSignInsert(filename=name,filepath=filename,sign=finalHash,appId=1)
+                    print("file")
+                    hash = self.getReadableHash(filename)
+                    print(hash)
+                    print(info)
+                    fileString = hash + str(info.st_size) + str(info.st_mtime) + filename
+                    print(fileString)
+                    fileString = fileString.encode('utf-8')
+                    print(fileString)
+                    finalHash = self.getSha256Hash(fileString)
+                    print(finalHash)
+                    sqlops.sqlSignInsert(filename=name,filepath=filename,sign=finalHash,appId=1)
 
-checkfolder('testdir')
+file = FileHash()
+file.checkfolder('testdir')
 
-
-
-#changeee
-
-#teswts
 
 
 
