b92e85f0f2e73a0ebaa21e7ad680b457734b028f
manukris <manukrishnatesro@gmail.com>
2019-03-06 17:25:15 +0530

    file hash folder recursineew

--- processlist.py
+++ processlist.py
@@ -1,11 +1,15 @@
 from psutil import *
 
-for proc in process_iter():
-   try:
-       pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
-   except NoSuchProcess:
-      print("exception")
-   else:
-      print(pinfo)
-      if pinfo['name'] == 'sublime_text':
-          proc.kill()
+class Processhandle:
+    appname = ""
+    def stopapp(self):
+        for proc in process_iter():
+           try:
+               pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+           except NoSuchProcess:
+              print("exception")
+           else:
+              print(pinfo)
+              if pinfo['name'] == 'sublime_text':
+                  proc.kill()
+
